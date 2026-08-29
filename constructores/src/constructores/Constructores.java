b422ace31eea24629fb283ea8a350703a525662d
Gissel BT <beltrantiqueelsygissel@gmail.com>
2026-08-29 13:16:23 -0500

    En este bloque de codigo estan creados los objetos y constructores de la clase videojuegos

--- constructores/src/constructores/Constructores.java
+++ constructores/src/constructores/Constructores.java
@@ -14,7 +14,13 @@ public class Constructores {
         Juguete objeto5 = new Juguete("Oso peluche", 49);
         Juguete objeto6 = new Juguete(89, 5.990);
         Juguete objeto7 = new Juguete("Balon", 89,2.00);
-        
+         Videojugos objeto8 =new Videojugos("Micraft");
+        Videojugos objeto9 = new Videojugos(3);
+        Videojugos objeto10 = new Videojugos(2.456);
+        Videojugos objeto11 = new Videojugos("Ajedrez", 4.500);
+        Videojugos objeto12 = new Videojugos("Hay Day", 1);
+        Videojugos objeto13 = new Videojugos(1, 5.990);
+        Videojugos objeto14 = new Videojugos("fifa", 7,2.00);
        
         
         
