17bffb72ac2758e4b8ebcbe3b459278b83b3b7e7
Gissel BT <beltrantiqueelsygissel@gmail.com>
2026-08-28 23:58:35 -0500

    Clase ProyectoSG completa

--- proyectoGS/src/proyectogs/ProyectoGS.java
+++ proyectoGS/src/proyectogs/ProyectoGS.java
@@ -2,12 +2,21 @@
 package proyectogs;
 
 
+
 public class ProyectoGS {
 
     
     public static void main(String[] args) 
-    {    
-        
+    {
+      Estudiante est1 = new Estudiante();
+      est1.setNombre("camaron");
+      est1.setDocumento("12345");
+      est1.setEdad(12);
+      est1.setCarrera("Veterinaria");
+      
+      PROFESOR profesor1 = new PROFESOR();
+      
+      
     }
     
 }
