9c4f2b1fb3a3102eba7da20fa07729ee91a324e3
Gissel BT <beltrantiqueelsygissel@gmail.com>
2026-08-29 00:11:56 -0500

    En en main inicializamos un objeto e icimos un encapsulamiento

--- proyectoGS/src/proyectogs/ProyectoGS.java
+++ proyectoGS/src/proyectogs/ProyectoGS.java
@@ -15,7 +15,11 @@ public class ProyectoGS {
       est1.setCarrera("Veterinaria");
       
       PROFESOR profesor1 = new PROFESOR();
-      
+       profesor1.setNombre("Molly");
+      profesor1.setDocumento("098345");
+      profesor1.setEdad(46);
+      profesor1.setMateria("Marketing Digital");
+      profesor1.setSalario(24.000);
       
     }
     
