df38d4db614df538f872a7185f5af1769c92b0d3
Gissel BT <beltrantiqueelsygissel@gmail.com>
2026-08-29 13:11:31 -0500

    En este bloque de codigo estan creados los objetos de la clase videojugos que recibe diferentes parametros

--- constructores/src/constructores/Videojugos.java
+++ constructores/src/constructores/Videojugos.java
@@ -14,4 +14,38 @@ public class Videojugos
     private int dificultad = 0;
     private double precio =0.0;
     
+
+    public Videojugos(String nombre)
+    {
+    this.nombre = nombre;
+    }
+    public Videojugos(int dificultad)
+    {
+    this.dificultad = dificultad;
+    }
+    public Videojugos(double precio)
+    {
+    this.precio=precio;
+    }
+    public Videojugos(String nombre, int dificultad)
+    {
+    this.nombre = nombre;
+    this.dificultad =dificultad;
+    }
+    public Videojugos(String nombre, double precio)
+    {
+    this.nombre = nombre;
+    this.precio = precio;
+    }
+    public Videojugos(int dificultad, double precio)
+    {
+    this.dificultad = dificultad;
+    this.precio = precio;
+    }
+    public Videojugos(String nombre,int dificultad,double precio )
+    {
+    this.nombre =nombre;
+    this.dificultad = dificultad;
+    this.precio = precio;
+    }    
 }
